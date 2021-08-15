195240b77b5b706d89ce1c8bd3939721d7572fb3
gianvittorio <gianvittorio.castellano@recargapay.com>
2021-08-14 23:26:14 -0300

    add users service

--- order-service/src/main/java/com/gianvittorio/orderservice/config/R2DBCConfiguration.java
+++ order-service/src/main/java/com/gianvittorio/orderservice/config/R2DBCConfiguration.java
@@ -4,17 +4,17 @@ import io.r2dbc.h2.H2ConnectionFactory;
 import io.r2dbc.spi.ConnectionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.r2dbc.config.AbstractR2dbcConfiguration;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
-import org.springframework.r2dbc.connection.R2dbcTransactionManager;
 import org.springframework.r2dbc.connection.init.CompositeDatabasePopulator;
 import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
 import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
-import org.springframework.transaction.ReactiveTransactionManager;
 
 @Configuration
 @EnableR2dbcRepositories
+@Profile("test")
 public class R2DBCConfiguration extends AbstractR2dbcConfiguration {
 
     @Override
@@ -22,7 +22,6 @@ public class R2DBCConfiguration extends AbstractR2dbcConfiguration {
     public ConnectionFactory connectionFactory() {
         return H2ConnectionFactory.inMemory("testdb");
     }
-
 
     @Bean
     public ConnectionFactoryInitializer initializer(final ConnectionFactory connectionFactory) {
