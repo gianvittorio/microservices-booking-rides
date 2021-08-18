f3dbe15a34eda5bb280359e2f7678ec99fad4395
gianvittorio <gianvittorio.castellano@recargapay.com>
2021-08-18 16:58:36 -0300

    successfully drop notification email

--- order-service/src/main/java/com/gianvittorio/orderservice/service/SchedulerService.java
+++ order-service/src/main/java/com/gianvittorio/orderservice/service/SchedulerService.java
@@ -1,22 +1,28 @@
 package com.gianvittorio.orderservice.service;
 
+import com.gianvittorio.orderservice.domain.entity.OrderEntity;
 import com.gianvittorio.orderservice.repository.OrdersRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.util.Collection;
 
 @Service
 @Log4j2
 @RequiredArgsConstructor
 public class SchedulerService {
 
+    private final JavaMailSender mailSender;
+
     private final OrdersRepository ordersRepository;
 
-    @Scheduled(fixedRate = 5000)//(cron = "0 * * * * *")
+    @Scheduled(cron = "0 * * * * *")
     public void schedule() {
 
         final LocalDateTime start = LocalDateTime.now(ZoneId.of("America/Sao_Paulo"));
@@ -24,7 +30,21 @@ public class SchedulerService {
 
         ordersRepository.findByStatusAndDepartureTimeBetween("pending", start, end)
                 .collectList()
-                .doOnNext(orderEntityList -> log.info("Accusi: {}, {}, {}", orderEntityList.size(), start, end))
+                .doOnNext(this::sendToAll)
                 .subscribe();
+    }
+
+    private void sendToAll(final Collection<? extends OrderEntity> orders) {
+
+        log.info("Accusi: {}", orders.size());
+
+        orders.forEach(order -> {
+            final SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom("tobeto1183@error57.com");
+            message.setTo("tobeto1183@error57.com");
+            message.setSubject("Notification");
+            message.setText(String.format("Id: %s; origin: %s; destination: %s; departure: %s", order.getId(), order.getOrigin(), order.getDestination(), order.getDepartureTime().toString()));
+            mailSender.send(message);
+        });
     }
 }
